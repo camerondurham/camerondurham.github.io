676d0c4a58420a1775517072ed6d2a6462a6855f
Cam <17013462+camerondurham@users.noreply.github.com>
2025-02-28 19:51:34 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,41 +6,26 @@
 
   outputs = { self, nixpkgs }:
     let
-      darwinPkgs = nixpkgs.legacyPackages.x86_64-darwin.pkgs;
-      m1DarwinPkgs = nixpkgs.legacyPackages.aarch64-darwin.pkgs;
-      linuxPkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-      # fooScript = pkgs.writeScriptBin "foo.sh" ''
-      #   #!/bin/sh
-      #   echo $FOO
-      # '';
+      supportedSystems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+
+      # Helper function to get packages for a specific system
+      forSystem = system: let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          default = pkgs.mkShell {
+            name = "Zola Build Environment";
+            buildInputs = with pkgs; [
+              zola
+              nixpkgs-fmt
+              nodePackages_latest.markdownlint-cli2
+            ];
+          };
+        };
     in
     {
-      # Run these with:
-      # nix develop
-      devShells.x86_64-darwin.default = darwinPkgs.mkShell {
-        name = "Zola Build Environment on Intel Macs";
-        buildInputs = [
-          darwinPkgs.zola
-          darwinPkgs.nixpkgs-fmt
-          darwinPkgs.nodePackages_latest.markdownlint-cli2
-        ];
-      };
-      # TODO: figure out a better way to handle this
-      devShells.aarch64-darwin.default = m1DarwinPkgs.mkShell {
-        name = "Zola Build Environment on Apple Silicon";
-        buildInputs = [
-          m1DarwinPkgs.zola
-          m1DarwinPkgs.nixpkgs-fmt
-          m1DarwinPkgs.nodePackages_latest.markdownlint-cli2
-        ];
-      };
-      devShells.x86_64-linux.default = linuxPkgs.mkShell {
-        name = "Zola Build Environment on Linux";
-        buildInputs = [
-          linuxPkgs.zola
-          linuxPkgs.nixpkgs-fmt
-          linuxPkgs.nodePackages_latest.markdownlint-cli2
-        ];
-      };
+      devShells = nixpkgs.lib.genAttrs supportedSystems forSystem;
     };
 }
